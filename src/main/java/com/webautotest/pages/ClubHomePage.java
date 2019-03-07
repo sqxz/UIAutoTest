1645f1e817aca8fd547439a699305efc5d21e971
sqxz <1176011893@qq.com>
2019-03-07 09:50:12 +0800

    优化报告

---
+++ src/main/java/com/webautotest/pages/ClubHomePage.java
@@ -0,0 +1,7 @@
+package com.webautotest.pages;
+
+import org.openqa.selenium.By;
+
+public class ClubHomePage {
+    public static By CLUBHOMEPAGE_H2_VERSION = By.xpath("//h2");
+}
--- src/main/java/com/webautotest/testcases/Login_002.java
+++ src/main/java/com/webautotest/testcases/Login_002.java
@@ -3,6 +3,7 @@ package com.webautotest.testcases;
 import com.webautotest.base.BaseParpare;
 import com.webautotest.pagehelper.HomePageHelper;
 import com.webautotest.pagehelper.LoginPageHelper;
+import com.webautotest.pages.ClubHomePage;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -15,7 +16,7 @@ public class Login_002 extends BaseParpare {
         HomePageHelper.switchToLoginPage();
         LoginPageHelper.doLogin(data.get("userName"),data.get("password"));
         seleniumUtil.pause(3);
-        Assert.assertEquals("","");
+        Assert.assertEquals(data.get("expected"), seleniumUtil.getText(ClubHomePage.CLUBHOMEPAGE_H2_VERSION));
     }
 
 }
